b616fc41f6de579e06d5b37968716c1b56d92ac6
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2022-02-10 15:23:56 +0100

    style: Delete semi-colon

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Import http module 
 const http = require('http')
 const handles = require('./handles')
-const server = http.createServer(handles.serverHandle);
+
+const server = http.createServer(handles.serverHandle)
 server.listen(8080)
