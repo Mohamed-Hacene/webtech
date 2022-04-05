7edb308fa49e69d2dc83d66810f945c63edce4ea
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2022-04-05 14:37:46 +0200

    feat: add mui style

--- packages/front-end/src/App.js
+++ packages/front-end/src/App.js
@@ -1,27 +1,40 @@
 
 /** @jsxImportSource @emotion/react */
+import { useState } from 'react'
+import { useTheme } from '@mui/styles';
 import './App.css';
 // Local
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Main from './components/Main'
+import Login from './Login'
 
-const styles = {
+const useStyles = (theme) => ({
   root: {
     boxSizing: 'border-box',
     display: 'flex',
     flexDirection: 'column',
     backgroundColor: '#565E71',
-    padding: '50px',
+    [theme.breakpoints.up('sm')]: {
+      padding: '50px',
+    },
   },
-}
+})
 
-export default () => {
+const App = () => {
+  const styles = useStyles(useTheme())
+  const [user, setUser] = useState(null)
+  const [drawerMobileVisible, setDrawerMobileVisible] = useState(false)
+  const drawerToggleListener = () => {
+    setDrawerMobileVisible(!drawerMobileVisible)
+  }
   return (
     <div className="App" css={styles.root}>
-      <Header />
-      <Main />
+      <Header drawerToggleListener={drawerToggleListener}/>
+      { user ? <Main drawerMobileVisible={drawerMobileVisible}/> : <Login setUser={setUser} /> }
       <Footer />
     </div>
   );
 }
+
+export default App
