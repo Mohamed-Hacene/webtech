4952dc7fb4f6e190e690eff1d83c0cb919b8d63b
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2022-04-05 16:26:20 +0200

    feat: display email on messages

--- packages/front-end/src/components/MessagesForm.js
+++ packages/front-end/src/components/MessagesForm.js
@@ -1,5 +1,6 @@
 
 /** @jsxImportSource @emotion/react */
+import { useCookies } from 'react-cookie';
 
 const styles = {
   form: {
@@ -26,12 +27,18 @@ const styles = {
 const MessagesForm = ({
   addMessage
 }) => {
+  const [cookies,,] = useCookies([])
+  let payload
+  if(cookies.oauth) {
+    const id_payload = cookies.oauth.id_token.split('.')[1]
+    payload = JSON.parse(atob(id_payload))
+  }
   const onSubmit = (e) => {
     e.preventDefault()
     const data = new FormData(e.target)
     addMessage({
       content: data.get('content'),
-      author: 'david',
+      author: payload ? payload.email : 'unauthorized',
       creation: Date.now(),
     })
     e.target.elements.content.value = ''
