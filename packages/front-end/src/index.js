1eae96639217f2b831efe070cfcf4b71a43af175
Mohamed-Hacene <mohamedhacene.b@gmail.com>
2022-04-05 14:36:39 +0200

    feat: add ThemeProvider

--- packages/front-end/src/index.js
+++ packages/front-end/src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "@fontsource/roboto";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
